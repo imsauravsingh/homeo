aba78a1c2b71e378ed3876c181328f6c2caea94a
tanushree bhunia <tanushree bhunia>
2016-09-11 11:11:01 +0530

    11-09-2016: overwrite com_homeo in template section

--- configuration.php
+++ configuration.php
@@ -20,7 +20,7 @@ class JConfig {
 	public $live_site = '';
 	public $secret = 'mhPotVap4nmHTX7J';
 	public $gzip = '0';
-	public $error_reporting = 'default';
+	public $error_reporting = 'none';
 	public $helpurl = 'https://help.joomla.org/proxy/index.php?keyref=Help{major}{minor}:{keyref}';
 	public $ftp_host = '';
 	public $ftp_port = '';
