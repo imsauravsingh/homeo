3738462ab2e3397c8999556861003f8a702be51a
tanushree bhunia <tanushree bhunia>
2016-09-14 10:37:16 +0530

    delted untracked files

--- administrator/components/com_easyappointment/uninstall.sql
+++
@@ -1,4 +0,0 @@
-DROP TABLE IF EXISTS `#__make_appointment_payments`;
-DROP TABLE IF EXISTS `#__make_appointment_reservations`;
-DROP TABLE IF EXISTS `#__make_appointment_services`;
-DROP TABLE IF EXISTS `#__make_appointment_staff`;
