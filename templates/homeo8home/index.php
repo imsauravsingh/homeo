1cff615d98044123e7264f71f428fd240dcff83e
tanushree bhunia <tanushree bhunia>
2016-09-24 12:50:01 +0530

    logo changes

--- templates/homeo8home/index.php
+++ templates/homeo8home/index.php
@@ -139,7 +139,7 @@ else
 // Logo file or site title param
 if ($this->params->get('logoFile'))
 {
-	$logo = '<img src="' . JUri::root() . '/images/homeo8home2.png' . '" alt="' . $sitename . '" />';
+	$logo = '<img src="' . JUri::root() . $this->params->get('logoFile') . '" alt="' . $sitename . '" />';
 	$logo2 = '<img src="' . JUri::root() . $this->params->get('logoFile') . '" style="width: ; margin-top: -1px;" alt="' . $sitename . '" />';
 }
 elseif ($this->params->get('sitetitle'))
